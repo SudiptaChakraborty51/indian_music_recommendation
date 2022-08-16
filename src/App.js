2eb0f5816345fed567390bbc7072a062fd5e141e
Sudipta <hisudipta26@gmail.com>
2022-08-17 01:38:40 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+import Bhairav from "./Music/Bhairav.mp3";
+import Asavari from "./Music/Asavari.mp3";
+import Bhupali from "./Music/Bhupali.mp3";
+import Baul from "./Music/Baul.mp3";
+import Bihu from "./Music/Bihu.mp3";
+import ElectroFolk from "./Music/ElectroFolk.mp3";
+import PyarHua from "./Music/PyarHua.mp3";
+import JeenaYahan from "./Music/JeenaYahan.mp3";
+import EkAjnabee from "./Music/EkAjnabee.mp3";
 
 function App() {
+  const [music, setMusic] = useState("Classical");
+  var musicRecommendationDB = {
+    Classical: [
+      {
+        title: "Bhairav",
+        rating: 4.5,
+        src: Bhairav
+      },
+      {
+        title: "Asavari",
+        rating: 4.3,
+        src: Asavari
+      },
+      {
+        title: "Bhupali",
+        rating: 4,
+        src: Bhupali
+      }
+    ],
+    Folk: [
+      {
+        title: "Baul- Bengal",
+        rating: 4.4,
+        src: Baul
+      },
+      {
+        title: "Electro Folk",
+        rating: 3.5,
+        src: ElectroFolk
+      },
+      {
+        title: "Bihugeet-Assam",
+        rating: 3.8,
+        src: Bihu
+      }
+    ],
+    Retro: [
+      {
+        title: "Pyar hua iqrar hua",
+        rating: 4.8,
+        src: PyarHua
+      },
+      {
+        title: "Jeena yahan marna yahan",
+        rating: 4.6,
+        src: JeenaYahan
+      },
+      {
+        title: "Ek Ajnabee haseena se",
+        rating: 4.5,
+        src: EkAjnabee
+      }
+    ]
+  };
+
+  var genreList = Object.keys(musicRecommendationDB);
+
+  function showMusicRecommendation(event) {
+    var recommendation = event.target.innerText;
+    setMusic(recommendation);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">Indian Music Recommendations</nav>
+      <div className="container">
+        <h1>🎶 Let's the music speak!</h1>
+        <p>Checkout my favourite musics. Select a genre to get started.</p>
+        {genreList.map((genre, index) => {
+          return (
+            <button
+              className="btn"
+              key={index}
+              onClick={showMusicRecommendation}
+            >
+              {genre}
+            </button>
+          );
+        })}
+        <hr style={{ margin: "2rem 0rem" }} />
+        <ul>
+          {musicRecommendationDB[music].map((genre, index) => {
+            return (
+              <li key={index}>
+                <p id="genre-title">{genre.title}</p>
+                <p id="genre-rating">{genre.rating}</p>
+                <audio controls src={genre.src}></audio>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
